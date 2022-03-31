23b5c20ddf5582976efa481db04868ce12337a02
Rubens Júnior <rubens.jr107@gmail.com>
2022-03-31 11:07:35 -0300

    updated main container

--- index.php
+++ index.php
@@ -8,17 +8,21 @@
 
   <!-- link bootstrap -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+  <!-- link css -->
+  <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
   <header>
     <?php include_once('./resources/layout/header.php'); ?>
   </header>
-  <main class="">
-    <div class="container border">
-      <div class="row mt-5">
-        <div class="col">
-          <h1>Bem-Vindo</h1>
-          <h3>Sistema de Gestão de Farmácia</h3>
+  <main class="w-100 position-relative">
+    <img src="./assets/images/logo UNICEPLAC.png" alt="Logo Uniceplac" class="position-absolute top-0 end-0 border">
+    <div class="container position-relative">
+      <div class="row">
+        <div class="col pt-5 position-absolute top-0 start-0 border">
+          <h1>BEM-VINDO</h1>
+          <h1>SISTEMA DE GESTÃO DE MATERIAS DE FARMÁCIA</h1>
         </div>
       </div>
     </div>
--- resources/layout/header.php
+++ resources/layout/header.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark">
   <div class="container justify-content-between">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
@@ -19,6 +19,8 @@
         </li>
       </ul>
     </div>
-    <div>Acessar</div>
+    <div>
+      <a href="#" class="text-light">Acessar</a>
+    </div>
   </div>
 </nav>
